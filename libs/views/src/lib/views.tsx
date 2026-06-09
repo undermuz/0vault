cb808cb50f795e43f72a2692beeb02dde2807f7f
Undermuz <undermuz@gmail.com>
2026-06-10 01:09:58 +0300

    initialize nx

---
+++ libs/views/src/lib/views.tsx
@@ -0,0 +1,11 @@
+import styles from './views.module.css';
+
+export function Views() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Views!</h1>
+    </div>
+  );
+}
+
+export default Views;
